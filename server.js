77d75ba7d184d1c896b477e03ebafe6e88ab89cf
Elisha Kay <kramer1346@gmail.com>
2017-12-04 21:11:28 +0200

    added more socket.io backend

--- server.js
+++ server.js
@@ -13,6 +13,47 @@ var schedule = require('node-schedule');
 var io = require('socket.io-client');
  
 
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('server running');
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html')
+// });
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log("connected: % of sockets connected", connections.length);
+
+    //Disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected % of sockets connected', connections.length);        
+    });
+
+    //Message
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+    
+    // new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users)
+    } 
+});
 
 var j = schedule.scheduleJob('42 * * * *', function(){
   console.log('The answer to life, the universe, and everything!');
