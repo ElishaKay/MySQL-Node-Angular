f952f82092f2ec9fbd3f7fc0a878ff71a88fbd35
Elisha Kay <kramer1346@gmail.com>
2017-12-11 21:17:44 +0200

    redirect

--- app/routes.js
+++ app/routes.js
@@ -16,50 +16,57 @@ module.exports = function(app,passport) {
     // Send email
 
 app.post('/send', (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Company: ${req.body.company}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'Mailgun',
-    auth: {
-        user: process.env.MAILGUN_USER, // generated ethereal user
-        pass: process.env.MAILGUN_PASSWORD  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-  });
+        // res.render('',{ message: req.flash('loginMessage') });
+       res.redirect('/');
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: '"Growth-X Team" <help@growth-x.com>', // sender address
-      to: req.body.email, // list of receivers
-      subject: 'Hey from GX', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
+  // const output = `
+  //   <p>You have a new contact request</p>
+  //   <h3>Contact Details</h3>
+  //   <ul>  
+  //     <li>Name: ${req.body.name}</li>
+  //     <li>Company: ${req.body.company}</li>
+  //     <li>Email: ${req.body.email}</li>
+  //     <li>Phone: ${req.body.phone}</li>
+  //   </ul>
+  //   <h3>Message</h3>
+  //   <p>${req.body.message}</p>
+  // `;
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // // create reusable transporter object using the default SMTP transport
+  // let transporter = nodemailer.createTransport({
+  //   service: 'Mailgun',
+  //   auth: {
+  //       user: process.env.MAILGUN_USER, // generated ethereal user
+  //       pass: process.env.MAILGUN_PASSWORD  // generated ethereal password
+  //   },
+  //   tls:{
+  //     rejectUnauthorized:false
+  //   }
+  // });
 
-      res.render('index', {msg:'Email has been sent'});
-  });
+  // // setup email data with unicode symbols
+  // let mailOptions = {
+  //     from: '"Growth-X Team" <help@growth-x.com>', // sender address
+  //     to: req.body.email, // list of receivers
+  //     subject: 'Hey from GX', // Subject line
+  //     text: 'Hello world?', // plain text body
+  //     html: output // html body
+  // };
+
+  // // send mail with defined transport object
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //         return console.log(error);
+  //     }
+  //     console.log('Message sent: %s', info.messageId);   
+  //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  //     
+  // });
+
+    
+
   });
 
 
--- config/database.js
+++ config/database.js
@@ -2,13 +2,25 @@
 
 // uncomment before pushing to git
 
+// module.exports = {
+//     'connection': {
+//         'host': process.env.HOST, // localde iseniz bu , local değilse ip adresini yazınız .
+//         'user': process.env.USER, // kullanıcı adı 
+//         'password': process.env.PASSWORD, // şifreniz 
+//         'database': process.env.DATABASE // database ismi .
+//     },
+// 	'database': process.env.DATABASE,
+    
+// };
+
+
 module.exports = {
     'connection': {
-        'host': process.env.HOST, // localde iseniz bu , local değilse ip adresini yazınız .
-        'user': process.env.USER, // kullanıcı adı 
-        'password': process.env.PASSWORD, // şifreniz 
-        'database': process.env.DATABASE // database ismi .
+        'host': 'localhost', // localde iseniz bu , local değilse ip adresini yazınız .
+        'user': 'root', // kullanıcı adı 
+        'password': 'pokemon7', // şifreniz 
+        'database': 'nodedb' // database ismi .
     },
-	'database': process.env.DATABASE,
+	'database': 'nodedb',
     
 };
