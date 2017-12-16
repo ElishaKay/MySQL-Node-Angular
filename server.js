29ff3efc74dd673a349cca2d4eb9eedce0e1336e
Elisha Kay <kramer1346@gmail.com>
2017-12-16 19:29:46 +0200

    separated email files

--- server.js
+++ server.js
@@ -148,6 +148,10 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+// routes ======================================================================
+require('./app/email-template.js')(app); // load our routes and pass in our app and fully configured passport
+
+
 // launch ======================================================================
 // app.listen(port);
 console.log('App is running on localhost:' + port);
