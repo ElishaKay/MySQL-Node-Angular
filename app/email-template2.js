5fc59797a3a9793ef3a583d2060daeec224d85b7
elishakay <kramer1346@gmail.com>
2018-01-18 00:23:11 +0200

    cleaned up

---
+++ app/email-template2.js
@@ -0,0 +1,60 @@
+
+
+
+// Just the code for sending based on User Input in the '/send' route
+
+app.post('/send', (req, res) => {
+
+// Defining variables
+
+    const output = '';
+
+
+// Sending email immediately based on User Input - like contact form - from client
+
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'Mailgun',
+    auth: {
+        user: process.env.MAILGUN_USER, // generated ethereal user
+        pass: process.env.MAILGUN_PASSWORD  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Growth-X Team" <help@growth-x.com>', // sender address
+      to: req.body.email, // list of receivers
+      subject: 'Hey from GX', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      
+  });
+});
+    
---
+++ app/scheduling.js
@@ -0,0 +1,24 @@
+You can also use arrays to specify a list of acceptable values, 
+and the Range object to specify a range of start and end values, 
+with an optional step parameter. For instance, this will print a message on Thursday, 
+Friday, Saturday, and Sunday at 5pm:
+
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(4, 6)];
+rule.hour = 17;
+rule.minute = 0;
+
+
+
+
+To make things a little easier, an object literal syntax is also supported, 
+like in this example which will log a message every Sunday at 2:30pm:
+
+var j = schedule.scheduleJob({hour: 14, minute: 30, dayOfWeek: 0}, function(){
+  console.log('Time for tea!');
+});
+
+
+
+      // End of scheduling
+
