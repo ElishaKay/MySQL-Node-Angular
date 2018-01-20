887a03a1afbb47de2c3926247ee22bf25f664304
elishakay <kramer1346@gmail.com>
2018-01-20 21:56:05 +0200

    Revert "added a for loop in email-template.js file"
    This reverts commit e4c8aec72e00e6f654dbba75d4b2c8970ed284ac.

--- app/email-template.js
+++ app/email-template.js
@@ -11,55 +11,58 @@ module.exports = function(app) {
 // Every day at 5 am, segment the audience that wants a specific daily update:
      
     var j = schedule.scheduleJob('42 * * * * *', function(){
-        // Step 1: Define the email that will appear as the sender of your emails
+
       console.log('The answer to life, the universe, and everything!');
-      // Step 2: Define the Segment that will be receiving your email every minute
 
-      senderEmail = 'elisha@growth-x.com'; 
-
-      connection2.query("select * from client where client_email like '%cifgiving%'", function (err, rows) {
+      connection2.query("select * from client where client_email like '%kramer1346%'", function (err, rows) {
       console.log('This is the result of the query on top',rows[0].client_email);   
 
-      // looping through results and sending email to each person in the audience
-            for (i = 0; i < rows.length; i++) { 
-                recipientEmail = rows[i].client_email;
-                sendmail(senderEmail, recipientEmail, rows[i].client_id);
-            };
-        });
+
+      senderEmail = 'alephmarketingpros@gmail.com';      
+      
+      console.log(rows);
+
+      
      });
 
-    var sendmail = function(senderEmail, recipientEmail, clientIdFromSql) { 
+    // res.render('',{ message: req.flash('loginMessage') });
+      
+    console.log('mailgun logic goes here with variables');
 
     // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-           service: 'Mailgun',
-           auth: {
-               user: process.env.MAILGUN_USER, // generated ethereal user
-               pass: process.env.MAILGUN_PASSWORD  // generated ethereal password
+      let transporter = nodemailer.createTransport({
+       service: 'Mailgun',
+       auth: {
+             user: process.env.MAILGUN_USER, // generated ethereal user
+             pass: process.env.MAILGUN_PASSWORD  // generated ethereal password
              },
-           tls:{
-           rejectUnauthorized:false
-               }
-        });
+        tls:{
+          rejectUnauthorized:false
+        }
+      });
 
 
-        // Pass in the client id here
-        connection2.query('select * from client where client_id = ?',clientIdFromSql, function (err, rows) {
+// Pass in the client id here - in this case 404 (Hannah)
+      connection2.query("select * from client where client_email like '%kramer1346%'", function (err, rows) {
         console.log('printing the kramer1346 object within the send mail function',rows[0].client_email);
-        var recipientEmail = rows[0].client_email;
+        recipientEmail = rows[0].client_email;
 
         const output = `
-         <p>Wassup `+ rows[0].client_email + `<---- This is the name associated with the account, taken from the sql results</p>
-         <p>Wassup `+ rows[0].client_password + `<---- And this is the client password taken from the sql results</p>
-        
+
+
+                                Wassup `+ rows[0].client_name + `<---- This is the name associated with the account, taken from the sql results</a>
+             
+
          `;
+
+     
 
       // setup email data with unicode symbols
       let mailOptions = {
-          from: '"Leesh" <'+senderEmail+'>', // sender address
+          from: '"GX" <'+senderEmail+'>', // sender address
           to: recipientEmail, // list of receivers
-          subject: 'Jan 19th - Home Test', // Subject line
-          text: rows[0].client_email, // plain text body
+          subject: 'Jan 18th- Ramat Gan Test', // Subject line
+          text: rows[0].client_name, // plain text body
           html: output // html body
               };
 
@@ -70,9 +73,14 @@ module.exports = function(app) {
               return console.log(error);
           }
           console.log('Message sent: %s', info.messageId);   
-          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));     
+          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+
+          
       });
-      // Closing second sql Query
+
      });
-// Closing sendmail function
- }};    
+
+    
+}
+)};
