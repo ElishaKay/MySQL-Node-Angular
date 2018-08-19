4a32f00051b37b3a61a9ecf539a04591506fc899
elishakay <kramer1346@gmail.com>
2018-08-19 21:38:44 +0300

    clean

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 module.exports = {
     'connection': {
-        'host': process.env.HOST, // localde iseniz bu , local değilse ip adresini yazınız .
-        'user': process.env.USER, // kullanıcı adı 
-        'password': process.env.PASSWORD, // şifreniz 
-        'database': process.env.DATABASE // database ismi .
+        'host': process.env.HOST,
+        'user': process.env.USER,  
+        'password': process.env.PASSWORD, 
+        'database': process.env.DATABASE 
     },
 
     // The second connection is for the db that you want to segment your audience from
     // and to pull data from for custom reports
     
     'connection2': {
-        'host': process.env.HOST2, // localde iseniz bu , local değilse ip adresini yazınız .
-        'user': process.env.USER2, // kullanıcı adı 
-        'password': process.env.PASSWORD2, // şifreniz 
-        'database': process.env.DATABASE2 // database ismi .
+        'host': process.env.HOST2, 
+        'user': process.env.USER2, 
+        'password': process.env.PASSWORD2, 
+        'database': process.env.DATABASE2
     }
 };
 
